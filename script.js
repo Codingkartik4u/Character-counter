6894c340a14e3ab24eafc216fc1df175bea30bc6
Codingkartik4u <codingkartik4u@gmail.com>
2025-02-21 18:57:54 +0530

    Add files via upload
    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let inputtext= document.getElementById('inputtext');
+let count= document.getElementById('count');
+inputtext.addEventListener('input', () => {
+    count.textContent = inputtext.value.length;
+})
